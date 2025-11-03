01bcd2188a0159fb2b191e08b453cf1547b1f905
Ezra Abrari <ezraabrari@gmail.com>
2025-11-03 09:12:47 +0700

    Membuat Read all mengguunakan post

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ db.sequelize.sync().then(() => {
         console.log(`✅ Server berjalan di http://localhost:${PORT}`);
     });
 }).catch(err => console.log("❌ Gagal konek DB:", err));
+
+
+app.post('/buku', async (req, res) => {
+    try {
+        const buku = await db.buku.create(req.body);
+        res.status(201).send(buku);
+    } catch (err) {
+        res.status(500).send({ message: "Gagal menambahkan data", error: err });
+    }
+});
+
+// ✅ READ ALL (GET)
+app.get('/buku', async (req, res) => {
+    try {
+        const buku = await db.buku.findAll();
+        res.send(buku);
+    } catch (err) {
+        res.status(500).send({ message: "Gagal mengambil data", error: err });
+    }
+});
